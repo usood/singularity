a361aa28f9e5473a9620a2f288199f421ea83a29
asoheili <tonyarash@gmail.com>
2015-10-05 22:03:40 -0400

    Add social share buttons for posts

--- client/helpers.js
+++ client/helpers.js
@@ -23,3 +23,13 @@ Handlebars.registerHelper('getUserEmail', function(user) {
   }
   return email;
 });
+
+UI.registerHelper('shareOnFacebookLink', function() {
+  return 'https://www.facebook.com/sharer/sharer.php?&u=' + window.location.href;
+});
+UI.registerHelper('shareOnTwitterLink', function() {
+  return 'https://twitter.com/intent/tweet?url=' + window.location.href + '&text=' + document.title;
+});
+UI.registerHelper('shareOnGooglePlusLink', function() {
+  return 'https://plus.google.com/share?url=' + window.location.href;
+});
