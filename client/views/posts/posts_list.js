fe10fa116f2947472653598be4528122d41990a8
usood <upahar.sood@gmail.com>
2016-01-31 01:11:42 +0530

    Added slug to fix on edit page

--- client/views/posts/posts_list.js
+++ client/views/posts/posts_list.js
@@ -1,6 +1,19 @@
+Template.posts_list.onCreated(function(){
+  this.selectedLanguage = new ReactiveVar();
+  this.selectedBook = new ReactiveVar();
+  this.selectedChapter = new ReactiveVar();
+});
+Template.posts_list.events({
+  'change select' : function( event, template ) {
+
+    var instance = Template.instance();
+
+    instance.posts_list.set( 'valueToFilter' , event.target.value );
+  }
+})
 Template.posts_list.helpers({
 
-  posts: function(language, aBook, aChapter) {
+  posts: function() {
 
     return Posts.find( {}, {sort: {publishedOn: -1} } );
   }
