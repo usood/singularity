ec7a6b63fb20a25eda77e55f73eef6b7b146a81e
usood <upahar.sood@gmail.com>
2016-01-30 19:51:05 +0530

    New Blog based app

--- server/main.js
+++ server/main.js
@@ -1,15 +1,14 @@
 // Uncomment the code below and fill in your information.
 // Then start Meteor in your project to create an initial admin user
 
-/*
+
 if (Meteor.users.find().count() === 0) {
   var userObj = {
       username: 'admin', 
-      email: 'admin@example.com', 
+      email: 'admin@eklavya.in',
       password: 'admin',
       profile: {name: 'Admin'},
       roles: 'admin',
     };
   Accounts.createUser(userObj);
 }
-*/
